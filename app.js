2c13097e5cba4201c8f6aa3257a84308fbd0534f
sleyder02 <36565701+sleyder02@users.noreply.github.com>
2026-05-27 05:59:35 +0000

    Clase 28 Pantalla de Candidatos

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const btnMensaje = document.getElementById('btnMensaje');
 const mensajeClase = document.getElementById('mensajeClase');
+const botonesPerfil = document.querySelectorAll('.btnPerfil');
+const detallePerfil = document.getElementById('detallePerfil');
+
 let contadorClicks = 0
 
 btnMensaje.addEventListener('click', () => {
     //mensajeClase.textContent = '¡Hola! Este es un mensaje de alerta.';
-    mensajeClase.textContent = 'Texto a mostrar al hacer Click' + contadorClicks;
+    mensajeClase.textContent = 'Texto a mostrar al hacer Click ' + contadorClicks;
     contadorClicks = contadorClicks + 1;
+});
+
+botonesPerfil.forEach((boton) => {
+    boton.addEventListener('click', () => {
+        const perfil = boton.getAttribute('data-perfil');
+        detallePerfil.textContent = `Informacion detallada del perfil: ${perfil}`;
+    });
 });
