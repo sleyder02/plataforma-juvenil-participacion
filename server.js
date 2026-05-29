ec2522e9e0bf4b99a838495b1bb92895f85a8abf
sleyder02 <36565701+sleyder02@users.noreply.github.com>
2026-05-29 12:05:47 +0000

    Actualizacion del archivo serve.js para la clase 30

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ app.use(express.json()); //para parsear el cuerpo de las solicitudes como JSON
 app.use(express.static(__dirname)); //leer todo tipo de archivos.
 
 const rutaCandidatos = path.join(__dirname, "data", "candidatos.json"); //ruta del archivo de datos
+const rutaVotos = path.join(__dirname, "data", "votos.json"); //ruta del archivo de votos path,join unir parte de una ruta
 // funcion que nos permite leer los candidatos desde el archivo JSON
+function leerVotos() {
+    const data = fs.readFileSync(rutaVotos, 'utf-8');
+    return JSON.parse(data); //lo convierte en arreglo de javascript
+}
+//funcion para guardar los votos en el archivo JSON, recibe un arreglo de votos y lo escribe en el archivo JSON
+function guardarVotos(votos) {
+    fs.writeFileSync(rutaVotos, JSON.stringify(votos, null, 2)); //escribir el arreglo de votos en el archivo JSON
+}
+
 function leerCandidatos() {
     
         const data = fs.readFileSync(rutaCandidatos, 'utf-8');
@@ -24,6 +34,45 @@ function guardarCandidatos(candidatos) {
     const candidatos = leerCandidatos();
     res.json(candidatos);
 }); //definir la ruta para obtener los candidatos
+
+app.get("/api/votos", function (req, res){
+    const votos = leerVotos();
+    res.json(votos);
+})
+
+//registrar un voto en el servidor
+app.post("/api/votos", function (req, res) {
+    const indentificacion = req.body.indentificacion;
+    const candidato = req.body.candidato;
+    if (!indentificacion || !candidato) {
+        return res.status(400).json({ error: "Faltan datos: identificacion o candidato" });
+    }
+        id: Date.now(); //generar un ID único basado en la fecha y hora.
+
+        const votos = leerVotos();
+
+        const yaVotos = votos.find(function (voto) {
+            return voto.indentificacion === indentificacion;
+        });
+        if (yaVotos) {
+            return res.status(400).json({ error: "Esta identificaciòn ya registrò un voto pedagògico" });
+        }
+        const nuevoVoto = {
+            id: Date.now(),
+            indentificacion: indentificacion,
+            candidato: candidato,
+            fecha: new Date().toISOString() // Agregar la fecha del voto
+        };
+
+        votos.push(nuevoVoto);
+        guardarVotos(votos);
+
+        res.status(201).json({
+            mensaje: "Voto pedagògico guardado correctamente",
+            voto: nuevoVoto
+        });
+});
+    // Verificar si el usuario ya ha votado
 
 // si queremos guardar nuevo candidato hacemos ruta POST
 app.post("/api/candidatos", function (req, res) {
@@ -49,7 +98,6 @@ app.post("/api/candidatos", function (req, res) {
         candidatos: nuevoCandidato
     }));
 }); //definir la ruta para guardar un nuevo candidato
-
 //iniciar el servidor para que pueda recivir solicitudes
 app.listen(PORT, function () {
     console.log("Servidor funcionando  en http://localhost:" + PORT);
